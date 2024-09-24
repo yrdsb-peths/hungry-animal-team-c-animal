21c9c132d1fb0a22865be2c7de27de52413b8176
Carlisa100 <348823873@gapps.yrdsb.ca>
2024-09-24 13:25:04 -0400

    Add Elephant and Apple Object

--- MyWorld.java
+++ MyWorld.java
@@ -1,22 +1,17 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MyWorld extends World
 {
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 300); 
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0); 
     }
     
     public void gameOverCode() 
