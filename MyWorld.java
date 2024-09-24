dd2091c31a07943efd977ff08ea74f8ccf207310
Carlisa100 <348823873@gapps.yrdsb.ca>
2024-09-24 13:28:36 -0400

    Create Apple Code

--- MyWorld.java
+++ MyWorld.java
@@ -12,11 +12,21 @@ public class MyWorld extends World
         
         Apple apple = new Apple();
         addObject(apple, 300, 0); 
+        
+        createApple();
     }
     
     public void gameOverCode() 
     {
         Label gameOverLabel = new Label ("Game Over", 100);
         addObject(gameOverLabel, 300, 200);
+    }
+    
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = Greenfoot.getRandomNumber(300);
+        addObject(apple, x, y); 
     }
 }
