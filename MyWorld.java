3b39825fc36e330351b097acfffaf5edae8491e4
TheLostone145 <martinthegamer145@gmail.com>
2024-09-25 12:37:14 -0400

    Fixed and added Apples

--- MyWorld.java
+++ MyWorld.java
@@ -2,6 +2,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class MyWorld extends World
 {
+    public int score=0;
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -21,12 +22,14 @@ public class MyWorld extends World
         Label gameOverLabel = new Label ("Game Over", 100);
         addObject(gameOverLabel, 300, 200);
     }
-    
+    public void increaseScore(){
+        score++; 
+    }
     public void createApple()
     {
         Apple apple = new Apple();
         int x = Greenfoot.getRandomNumber(600);
-        int y = Greenfoot.getRandomNumber(300);
+        int y = Greenfoot.getRandomNumber(1);
         addObject(apple, x, y); 
     }
 }
