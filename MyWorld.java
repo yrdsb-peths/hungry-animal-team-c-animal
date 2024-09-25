6aa2af8e4e39684c0a1a186d2f0a257427c19171
TheLostone145 <martinthegamer145@gmail.com>
2024-09-25 12:40:33 -0400

    Fixed double Apples

--- MyWorld.java
+++ MyWorld.java
@@ -6,13 +6,11 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1, false); 
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 300); 
         
-        Apple apple = new Apple();
-        addObject(apple, 300, 0); 
         
         createApple();
     }
